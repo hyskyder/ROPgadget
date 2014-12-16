f8340052d46a6d830250fd653b629446266a6b4e
XiaofanZhang <banban0802@gmail.com>
2014-12-16 16:36:22 +0100

    add instructions

--- ropgadget/ropchain/expression.py
+++ ropgadget/ropchain/expression.py
@@ -19,28 +19,67 @@ class Expression:
 			Expression Binary-op Expression
 
 		Binary-op:
-			&& || + - % & |  ~ ^ >> << == != > >= < <=
+			&& || + - % & |  ~ ^ >> << == != > >= < <= = 
 
 		Conditional-Expression:
 			Expression ? Expression : Expression;
 			
 	'''
+
 	def __init__(self, left, op=None, right=None, condition=None):
-		if conditon != None:
-			self.__left = left
-			self.__right = right
-			self.__condition = condition
+		if condition != None:
+			self.left = left
+			self.right = right
+			self.condition = condition
 		else:
-			self.__left = left
-			if op != None:
-				self.__right = right
-				self.__op = op
-	
+                        self.op = op
+			self.left = left
+                        self.right = right
+                        self.condition = condition 
+
+        def __str__(self):
+            if self.condition is not None:
+                return "("+str(self.condition)+"?" + str(self.left) + ":" + str(self.right)+")"
+            else:
+                if self.right is not None:
+                    return "(" + str(self.left)+ self.op + str(self.right) + ")"
+                elif self.op is not None:
+                    return  self.op + str(self.left) 
+                else:
+                    return str(self.left)
+
+        def binding(self, mapping):
+            if type(self.left) is Expression:
+                self.left.binding(mapping)
+            if type(self.right) is Expression:
+                self.right.binding(mapping)
+            if type(self.condition) is Expression:
+                self.condition.binding(mapping)
+            
+            for k,v in mapping.items():
+                if k == self.left:
+                    self.left = v
+                if k == self.right:
+                    self.right = v
+                if k == self.condition:
+                    self.condition = v
+
 	def reduce(self):
-		pass
+            pass
 
-
-
-
-
-
+if __name__ == '__main__':
+    a = Expression(1,"-")
+    b = Expression("EAX")
+    b.binding({"EAX":Expression("EAX", "+", 4)})
+    print b
+    c = Expression("EBX", "==", 4)
+    print c
+    d = Expression(a, "conditon", b, c)
+    print d
+    print Expression(4, "+" , 1)
+    print Expression(a, "^" , 1)
+    e = Expression("b", "&" , 0xffff)
+    print e
+    e.binding({"b":b})
+    print e
+    print Expression( Expression("EAX", ">>" , d ) , "+" , Expression("c", "-" ,1) )
