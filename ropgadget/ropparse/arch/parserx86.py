c6f7748f68cdabb8ce3bdac83b51ab3059d763fe
晓帆张 <xiaofanzhang@xiaofanzhangs-MacBook-Pro.local>
2014-12-18 22:26:58 +0100

    fix comment, ''' should not be used

--- ropgadget/ropparse/arch/parserx86.py
+++ ropgadget/ropparse/arch/parserx86.py
@@ -23,114 +23,106 @@ class X86:
 	    "cmovle": [2, ["operand1 = ( ( ( SF xor OF ) or ZF ) == 1) ? operand2 : operand1"]], 
 	    "cmovs": [2, ["operand1 = ( SF == 1 ) ? operand2 : operand1"]], 
 	    "cmovp": [2, ["operand1 = ( PF == 1 ) ? operand2 : operand1"]], 
-'''
-	    "xchg": [2], 
-	    "bswap": [2], 
-	    "xadd": [2], 
-	    "cmpxchg": [2], 
-'''
+#	    "xchg": [2], 
+#	    "bswap": [2], 
+#	    "xadd": [2], 
+#	    "cmpxchg": [2], 
 	    "push": [1, ["* sp = operand1", "sp = sp - length"]],
 	    "pop": [1, ["operand1 = * sp", "sp = sp + length"]], 
-'''
-	    "in": [0], 
-	    "out": [0], 
-	    "cwde": [1], 
-	    "cdq": [1], 
-	    "movsx": [2], 
-	    "movzx", [2],
+#	    "in": [0], 
+#	    "out": [0], 
+#	    "cwde": [1], 
+#	    "cdq": [1], 
+#	    "movsx": [2], 
+#	    "movzx", [2],
             # flag control instuctions
-            "stc": [0, "CF = 1"], 
-	    "clc": [0, "CF = 0"], 
-	    "cmc": [0, "CF = ~ CF"], 
-	    "cld": [0, "DF = 0"], 
-	    "std": [0, "DF = 1"], 
-	    "lahf": [0], 
-	    "shf": [0], 
-	    "pushfq": [1], 
-	    "popfq": [1], 
-	    "sti": [0], 
-	    "cli": [0],
-            # arithmetic
-	    "cmp": [2, ], 
-	    "daa": [1],  
-	    "das": [1], 
-	    "aaa": [1], 
-	    "aas": [1], 
-	    "aam": [1], 
-	    "aad":[1],
-'''
+            "stc": [0, ["CF = 1"]], 
+	    "clc": [0, ["CF = 0"]], 
+	    "cmc": [0, ["CF = ~ CF"]], 
+	    "cld": [0, ["DF = 0"]], 
+	    "std": [0, ["DF = 1"]], 
+#	    "lahf": [0], 
+#	    "shf": [0], 
+#	    "pushfq": [1], 
+#	    "popfq": [1], 
+#	    "sti": [0], 
+#	    "cli": [0],
+#            # arithmetic
+#	    "cmp": [2, ], 
+#	    "daa": [1],  
+#	    "das": [1], 
+#	    "aaa": [1], 
+#	    "aas": [1], 
+#	    "aam": [1], 
+#	    "aad":[1],
 	    "add": [2, ["operand1 = operand1 + operand2"]], 
 	    "adc": [2, ["operand1 = operand1 + operand2 + ( ( CF == 1 ) ? 1 : 0 )"]], 
 	    "sub": [2, ["operand1 = operand1 - operand2"]], 
 	    "sbb": [2, ["operand1 = operand1 - operand2 - ( ( CF == 1 ) ? 1 : 0 )"]], 
-'''
-	    "imul": [1], 
-	    "mul": [1], 
-	    "idiv": [1, "eax = ( edx << 32 + eax ) / operand1", "edx = edx:eax % operand1"], 
-	    "div": [1], 
-'''
-	    "inc": [1, ["operand1 = operand1 + 1"]], 
-	    "dec": [1, ["operand1 = operand1 - 1"]], 
-	    "neg": [1, ["operand1 = - operand1"]], 
+#	    "imul": [1], 
+#	    "mul": [1], 
+#	    "idiv": [1, "eax = ( edx << 32 + eax ) / operand1", "edx = edx:eax % operand1"], 
+#	    "div": [1], 
+#	    "inc": [1, ["operand1 = operand1 + 1"]], 
+#	    "dec": [1, ["operand1 = operand1 - 1"]], 
+#	    "neg": [1, ["operand1 = - operand1"]], 
             # control transfer
             "ret": [1, ["ip = * sp", "sp = sp + length"]], 
-'''
-	    "iret": [1], 
-	    "int": [0], 
-	    "into": [0], 
-	    "enter": [0], 
-	    "leave": [0], 
-	    "call": [1], 
-	    "jmp": [1], 
-	    "ja": [1], 
-	    "jae": [1], 
-	    "jb": [1], 
-	    "jbe": [1], 
-	    "jc": [1], 
-	    "je": [1], 
-	    "jnc": [1], 
-	    "jne": [1], 
-	    "jnp": [1], 
-	    "jp": [1], 
-	    "jg": [1], 
-	    "jge": [1], 
-	    "jl": [1], 
-	    "jle": [1], 
-	    "jno": [1], 
-	    "jns": [1], 
-	    "jo": [1], 
-	    "js": [1], 
-'''
+#	    "iret": [1], 
+#	    "int": [0], 
+#	    "into": [0], 
+#	    "enter": [0], 
+#	    "leave": [0], 
+#	    "call": [1], 
+#	    "jmp": [1], 
+#	    "ja": [1], 
+#	    "jae": [1], 
+#	    "jb": [1], 
+#	    "jbe": [1], 
+#	    "jc": [1], 
+#	    "je": [1], 
+#	    "jnc": [1], 
+#	    "jne": [1], 
+#	    "jnp": [1], 
+#	    "jp": [1], 
+#	    "jg": [1], 
+#	    "jge": [1], 
+#	    "jl": [1], 
+#	    "jle": [1], 
+#	    "jno": [1], 
+#	    "jns": [1], 
+#	    "jo": [1], 
+#	    "js": [1], 
             # logic
             "and": [2, ["operand1 = operand1 & operand2"]], 
 	    "or": [2, ["operand1 = operand2 | operand2"]], 
 	    "xor": [2, ["operand1 = operand1 ^ operand2"]], 
-	    "not": [2, ["operand1 = ~ operand1"]]}
+	    "not": [2, ["operand1 = ~ operand1"]]
             # shift and rotate
-'''
-            "sar": [2], 
-	    "shr": [2], 
-	    "sal": [2], 
-	    "shrd": [2], 
-	    "shld": [2], 
-	    "ror": [2], 
-	    "rol": [2],
-	    "rcr": [2], 
-	    "rcl": [2],
-            # bit and bytes
-            "bt", "bts", "btr", "btc", "bsf", "bsr", "sete", "setne", "seta", "setae", "setb", "setbe", "setg", "setge", "setl", "setle", "sets", "setns", "seto", "setno", "setpe", "setpo", "test",
-            # segment 
-            "lds": [0], 
-	    "les": [0], 
-	    "lfs": [0], 
-	    "lgs": [0], 
-	    "lss": [0],
-            # others
-            "lea": [2, "operand1 = & operand2"], 
-	    "nop": [0, ""], 
-	    "xlatb": [1]
-	    # TODO: string operation, loop operation, MMX instruction, float point, System instruction
-'''
+#            "sar": [2], 
+#	    "shr": [2], 
+#	    "sal": [2], 
+#	    "shrd": [2], 
+#	    "shld": [2], 
+#	    "ror": [2], 
+#	    "rol": [2],
+#	    "rcr": [2], 
+#	    "rcl": [2],
+#            # bit and bytes
+#            "bt", "bts", "btr", "btc", "bsf", "bsr", "sete", "setne", "seta", "setae", "setb", "setbe", "setg", "setge", "setl", "setle", "sets", "setns", "seto", "setno", "setpe", "setpo", "test",
+#            # segment 
+#            "lds": [0], 
+#	    "les": [0], 
+#	    "lfs": [0], 
+#	    "lgs": [0], 
+#	    "lss": [0],
+#            # others
+#            "lea": [2, "operand1 = & operand2"], 
+#	    "nop": [0, ""], 
+#	    "xlatb": [1]
+#	    # TODO: string operation, loop operation, MMX instruction, float point, System instruction
+#
+}
 class ROPParserX86:
 	def __init__(self, gadgets, mode):
 		self.gadgets = gadget
@@ -194,9 +186,10 @@ class ROPParserX86:
 		
 
 if __name__ == '__main__':
-    gadget = ["adc al, -0x6f ; mov eax, dword ptr [ecx + eax*4] ; sub eax, edx ; ret"]
+    gadget = ["add eax, -0x6f ; mov eax, dword ptr [ecx + eax*4] ; sub eax, edx ; ret"]
     p = ROPParserX86(gadget, CS_MODE_32)
     formulas = p.parse()
+    print gadget
     for formula in formulas:
 	    for reg, exp in formula.items():
-		    print reg, exp
+		    print reg , " ==> " ,  exp
